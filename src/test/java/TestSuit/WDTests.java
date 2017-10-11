16eed4b07adc034d79927afee17af9c021b13e17
workfrolov <work.frolov@gmail.com>
2017-10-11 12:11:18 +0300

    WD click test

--- src/test/java/TestSuit/WDTests.java
+++ src/test/java/TestSuit/WDTests.java
@@ -1,13 +1,18 @@
 package TestSuit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WDTests {
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.random.org");
-        driver.quit();
+        Thread.sleep(20000);
+        WebElement generate = driver.findElement(By.id("true-random-integer-generator-button"));
+        generate.click();
+
     }
 }
